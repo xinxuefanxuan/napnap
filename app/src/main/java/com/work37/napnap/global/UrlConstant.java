4966884d1707f65d593e84786937d5669b38c28e
Tlaerk <w18942071329@outlook.com>
2024-06-21 14:27:56 +0800

    图片上传

--- app/src/main/java/com/work37/napnap/global/UrlConstant.java
+++ app/src/main/java/com/work37/napnap/global/UrlConstant.java
@@ -1,5 +1,5 @@
 package com.work37.napnap.global;
 
 public interface UrlConstant {
-    String baseUrl = "https://cb76-223-104-76-218.ngrok-free.app/";
+    String baseUrl = "https://0f2b-223-104-76-218.ngrok-free.app/";
 }
